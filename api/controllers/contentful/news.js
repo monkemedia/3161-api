f4908e7f7a7d1261d2cc7f9b0cc8b5b1933fa697
monkemedia <info@monkemedia.co.uk>
2019-02-14 20:38:39 +0000

    Added a search and tag query to news api

--- api/controllers/contentful/news.js
+++ api/controllers/contentful/news.js
@@ -2,12 +2,16 @@ const appRoot = require('app-root-path')
 const client = require(appRoot + '/api/utils/initClient.js')
 
 exports.news = (req, res, next) => {
-  const limit = req.query.limit ? req.query.limit : 100
+  const limit = req.query.limit
+  const tag = req.query.tag
+  const search = req.query.search
 
    client.initClient()
     .getEntries({
       'content_type': 'news',
-      'limit': limit
+      'limit': limit || 100,
+      'fields.tags': tag || null,
+      'query': search || null
     })
       .then(entries => {
         const newArray = [];
